4ee79e4d2ab918fc761755f17fb6a58afcac45b3
kanttanhed <plinio.salazar@yahoo.com.br>
2021-05-27 12:33:32 -0300

    API Consuming

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <div id="app">
-    
+    <div v-for="(poke,index) in pokemons" :key="index">
+      <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/>
+   
+  </div>
   </div>
 </template>
 
 <script>
 
+import axios from 'axios';
+import Pokemon from './components/Pokemon'
 
 export default {
   name: 'App',
-  
+  data(){
+    return{
+      pokemons: []
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then( res => {
+      console.log("Consuming the pokemon list");
+       
+     this.pokemons = res.data.results;
+    })
+  },
+  components:{
+    Pokemon
+  } 
 }
 </script>
 
